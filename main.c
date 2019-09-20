5178e9bf850f66ce4f4a439d7fa3d40387ed0b26
unknown <donatien_beneat@yahoo.fr>
2019-09-20 16:51:42 +0200

    implementation of saveBMP

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
-struct rgb
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct rgb
 {
     float r,g,b;
-}
+} rgb;
 
 
 void saveBMP(char* path, int w, int h, int dpi, rgb* imageBuffer)
 {
+    FILE* img;
+    int img_size = w*h;
+    int file_size = 54+4*img_size;
+    int ppm = dpi*39.375;
+
+
     struct BMP_file_header
     {
         unsigned char   BMP_type[2];// 2 bytes
@@ -18,7 +28,7 @@ void saveBMP(char* path, int w, int h, int dpi, rgb* imageBuffer)
 
     struct BMP_image_header
     {
-        unsigned int    size_header;    // 4 bytes
+        unsigned int    header_size;    // 4 bytes
         unsigned int    w;              // 4 bytes
         unsigned int    h;              // 4 bytes
         short int       planes;         // 2 bytes
@@ -32,7 +42,82 @@ void saveBMP(char* path, int w, int h, int dpi, rgb* imageBuffer)
                                         //---TOTAL: 40 bytes
     } ih;
 
+    memcpy(&fh.BMP_type, "BM", 2);
+    fh.file_size        = file_size;
+    fh.reserved1        = 0;
+    fh.reserved2        = 0;
+    fh.offset_bits      = 0;
+
+    ih.header_size      = sizeof(ih);
+    ih.w                = w;
+    ih.h                = h;
+    ih.planes           = 1;
+    ih.bit_count        = 24;
+    ih.compression      = 0;
+    ih.img_size         = img_size;
+    ih.ppm_x            = ppm;
+    ih.ppm_y            = ppm;
+    ih.clr_used         = 0;
+    ih.clr_important    = 0;
+
+    img = fopen(path, "wb");
+    fwrite(&fh, 1, 14, img);
+    fwrite(&ih, 1, sizeof(ih), img);
+
+    for(int i=0; i<img_size; i++)
+    {
+        rgb pixel = imageBuffer[i];
+        float r = pixel.r;
+        float g = pixel.g;
+        float b = pixel.b;
+        unsigned char color[3] = {b, g, r};
+        fwrite(color, 1, sizeof(color), img);
+
+
+
+    }
+
+
+    fclose(img);
 
 
 }
+
+
+
+
+
+
+
+int main(void)
+{
+
+
+    int width  = 400,
+        height = 400,
+        dpi = 96;
+
+    rgb* pixels = malloc(width * height * sizeof(rgb));
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            int a = y * width + x;
+
+            if ((x > 50 && x < 350) && (y > 50 && y < 350)) {
+                pixels[a].r = 255;
+                pixels[a].g = 255;
+                pixels[a].b = 5;
+            } else {
+                pixels[a].r = 55;
+                pixels[a].g = 55;
+                pixels[a].b = 55;
+            }
+        }
+    }
+
+    saveBMP("black_border.bmp", width, height, dpi, pixels);
+
+    return 0;
+}
+
 
